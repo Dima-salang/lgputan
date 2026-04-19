c8987e2eae9d54961651863c30c7fcd4d8539034
Dima-salang <luisgabrielle1026@gmail.com>
2026-04-19 15:36:48 +0800

    added createCallerFactory for trcp to call server side funcs

--- src/integrations/trpc/init.ts
+++ src/integrations/trpc/init.ts
@@ -1,9 +1,16 @@
 import { initTRPC } from '@trpc/server'
 import superjson from 'superjson'
 
-const t = initTRPC.create({
+import type { Db } from '../../db'
+
+export interface Context {
+  db: Db
+}
+
+const t = initTRPC.context<Context>().create({
   transformer: superjson,
 })
 
 export const createTRPCRouter = t.router
 export const publicProcedure = t.procedure
+export const createCallerFactory = t.createCallerFactory
